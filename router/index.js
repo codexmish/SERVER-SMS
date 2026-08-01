7fafa1405fa5808b43551f2a14708c908a7f25db
codexmish <majharulislam.shanto.70@gmail.com>
2026-08-01 12:08:18 +0600

    limit set for auth routes

--- router/index.js
+++ router/index.js
@@ -3,8 +3,17 @@ const envConfig = require("../helpers/envConfig");
 const router = express.Router();
 const authRoute = require("./authRouter");
 const adminRouter = require("./adminRouter");
+const rateLimit = require("express-rate-limit");
 
-router.use(envConfig.BASE_URL, authRoute);
+// ----------set api req limit
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  limit: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: { error: "Too many requests, please try again later." },
+  skipSuccessfulRequests: true,
+});
+
+router.use(envConfig.BASE_URL, limiter, authRoute);
 router.use(envConfig.BASE_URL, adminRouter);
 
 module.exports = router;
